767d6a2af8e6d36bb35cd656af15c4241a451458
danibza92 <159460030+danibza92@users.noreply.github.com>
2024-02-09 10:49:56 +0100

    Update streamlit_app.py
    Hacer que la lista filtre lo seleccionado

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
+# Importamos streamlit que es un paquete de python
 import streamlit
 
+## Comandos de texto de streamlit
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfat Favorite')
 
@@ -10,13 +12,16 @@ streamlit.text('🥑🍞 Avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# Importamos la libreria pandas de python
 import pandas
 
+# Leemos el fichero de frutas de la nuve de S3
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Seteamos como indice el campo de frutas
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
