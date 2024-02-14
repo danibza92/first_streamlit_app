fe0a70ae6c13395c584713cec9f12b540f8e47e8
danibza92 <159460030+danibza92@users.noreply.github.com>
2024-02-14 13:44:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # Se normaliza en formato tabla el archivo de JSON que viene de la llamada a la api de fruityvice
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # se pintq el df obtenido
