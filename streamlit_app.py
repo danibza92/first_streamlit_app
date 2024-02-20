63666340090f25ffbc59e9487bbbb5020a5807f8
danibza92 <159460030+danibza92@users.noreply.github.com>
2024-02-20 11:20:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,10 +72,10 @@ if streamlit.button('Get Fruit Load List'):
       my_data_rows= get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
 
-add_fruit = streamlit.text_input('What fruit would you like to add')
-streamlit.write('The user entered ', add_fruit)
+#add_fruit = streamlit.text_input('What fruit would you like to add')
+#streamlit.write('The user entered ', add_fruit)
 
-my_cur.execute ("insert into fruit_load_list values ('from streamlit')");
+#my_cur.execute ("insert into fruit_load_list values ('from streamlit')");
 
 
 
